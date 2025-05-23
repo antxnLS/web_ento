fca871ce77955c36676123746cdbfdf592db4842
antxnLS <antxnlasalle@gmail.com>
2025-05-23 09:16:31 +0200

    web con enlaces

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
+import { Link, Routes, Route } from 'react-router-dom';
 import logo from './Benet.webp';
 import './App.css';
+import Page4 from './App-copy'; // 👈 Importamos la nueva página
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ul class="navbar">
-          <li><a class="active" href="App.js">index</a></li>
-          <li><a href="index.html">4</a></li>
-          <li><a href="">5</a></li>
-          <li><a href="">6</a></li>
+        <ul className="navbar">
+          <li><Link className="active" to="/">index</Link></li>
+          <li><Link to="/App-copy">4</Link></li>
+          <li><Link to="/page5">5</Link></li>
+          <li><Link to="/page6">6</Link></li>
         </ul>
-        <p>
-          Hola, soc John Caminant
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bottom Text
-        </p>
-        <a
-          className="App-link"
-          href="https://caminant.fandom.com/es/wiki/Benet_caminant"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Clica aqui para que entre un virus
-        </a>
 
-        <a
-          className="App-link"
-          href="./pagina.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Clica aqui para que no te entre un virus
-        </a>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <p>Hola, soc John Caminant</p>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Bottom Text</p>
+              <a
+                className="App-link"
+                href="https://caminant.fandom.com/es/wiki/Benet_caminant"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Clica aqui para que entre un virus
+              </a>
+              <a
+                className="App-link"
+                href="./pagina.html"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Clica aqui para que no te entre un virus
+              </a>
+            </>
+          } />
+          <Route path="/page4" element={<Page4 />} />
+          {/* puedes agregar más páginas si quieres */}
+        </Routes>
       </header>
     </div>
   );
